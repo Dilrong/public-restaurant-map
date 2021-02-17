9ce296d8d7a7b8f0c1eabf1e6c9819b98e264409
dilrong <wet932@naver.com>
2021-02-17 19:00:33 +0900

    Update: Feed Detail

--- src/scenes/home/index.js
+++ src/scenes/home/index.js
@@ -1,103 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, SafeAreaView, Platform, Image, View, FlatList, TouchableOpacity, StatusBar, Linking } from 'react-native';
-import { H6, BODY1, CAPTION } from '_styles/typography'
-import { SCALE_8 } from '_styles/spacing'
-import { scaleSize } from '_styles/mixins'
-import { GRAY_DARK } from '_styles/colors'
-import Axios from 'axios'
-import * as Analytics from 'expo-firebase-analytics'
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const FeedItem = ({ item }) => {
-  return (
-    <TouchableOpacity style={styles.feed} onPress={() => {
-      Linking.openURL(item.fields.URL)
-      Analytics.logEvent('click_feed', { item: item.fields.Title });  
-    }}>
-    <Image style={styles.feedImg} source={{uri: item.fields.Image }}/>
-    <View style={styles.feedContainer}>
-      <Text style={styles.feedTitle}>{item.fields.Title}</Text>
-      <Text style={styles.feedContent}>{item.fields.Content}</Text>
-    </View>
-    </TouchableOpacity>
-  )
-}
+import ListScreen from './list';
+import DetailScreen from './detail';
 
-const renderItem = ({ item }) => <FeedItem item={item}/>;
+const FeedStack = createStackNavigator();
 
-const HomeScreen = ({navigation}) => {
-  const [rows, setRows] = useState([])
-  const [isLoading, setLoading] = useState(true)
+const FeedStackScreen = ({navigation}) => (
+    <FeedStack.Navigator>
+        <FeedStack.Screen name="list" component={ListScreen} options={{headerShown:false}}/>
+        <FeedStack.Screen name="detail" component={DetailScreen} options={{
+            headerTitle: '상세'
+        }}/>
+    </FeedStack.Navigator>
+)
 
-  useEffect(() => {
-    Axios.get("https://api.airtable.com/v0/appiQGv4bZRpA1GnH/Feed?api_key=keyXGbMKu4hxmnPjr")
-    .then(res => {
-      setRows(res.data.records)
-      setLoading(false)
-    })
-    .catch(err => {
-      console.log(err)
-      setLoading(false)
-    })
-  })
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>홈</Text>
-      {isLoading? <Text style={styles.loader}>당신을 추천 콘텐츠 찾는 중</Text>:
-      <FlatList data={rows} renderItem={renderItem} keyExtractor={item => item.id}/>}
-    </SafeAreaView>
-  )
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-  title: {
-    width: "100%",
-    margin: SCALE_8,
-    fontSize: H6
-  },
-  loader: {
-    margin: SCALE_8,
-    fontSize: BODY1
-  },
-  carousel: {
-    width: '100%',
-    height: scaleSize(150),
-    resizeMode: "contain"
-  },
-  feed: {
-    margin: SCALE_8,
-    borderRadius: scaleSize(10),
-    borderWidth: scaleSize(0.5),
-    borderColor: '#d6d7da',
-  },
-  feedImg: {
-    width: '100%',
-    borderTopLeftRadius: scaleSize(10),
-    borderTopRightRadius: scaleSize(10),
-    height: scaleSize(150),
-    padding: SCALE_8
-  },
-  feedContainer: {
-    padding: SCALE_8,
-  },
-  feedTitle: {
-    fontSize: H6,
-    marginBottom: SCALE_8,
-    fontWeight: "bold"
-  },
-  feedContent: {
-    marginBottom: SCALE_8,
-    color: GRAY_DARK
-  },
-  feedTime: {
-    fontSize: CAPTION,
-    color: GRAY_DARK
-  }
-});
-
-export default HomeScreen;
+export default FeedStackScreen;
